a55ef416f1579f3f7f0cac96958873f9eb3ec855
Andrew Waller <andrewhwaller@gmail.com>
2019-04-27 08:48:00 +0000

    Automatically backed up by Learn

--- db/migrate/20190427084500_add_password_digest.rb
+++ db/migrate/20190427084500_add_password_digest.rb
@@ -1,7 +1,11 @@
 class AddPasswordDigest < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :password_digest do |t|
       t.string :password_digest
     end
+  end
+
+  def down
+    drop_table :users
   end
 end
