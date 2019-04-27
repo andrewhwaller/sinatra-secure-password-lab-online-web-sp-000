cb91ad8b85f0ba7b1cb05b7f2271ba855c55b50c
Andrew Waller <andrewhwaller@gmail.com>
2019-04-27 08:57:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ end
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to "/success"
+      redirect to "/account"
     else
       redirect to "/failure"
     end
