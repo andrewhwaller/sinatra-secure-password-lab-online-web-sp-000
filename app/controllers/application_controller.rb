bbca2bf439add02ffdbb3cda10d78340bdfc13a2
Andrew Waller <andrewhwaller@gmail.com>
2019-04-27 08:57:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-      redirect to "/login"
-    else
-      redirect "/failure"
-    end
+  if params[:username] == "" || params[:password] == ""
+    redirect '/failure'
+  else
+    User.create(username: params[:username], password: params[:password])
+    redirect '/login'
   end
+
+end
 
   get '/account' do
     @user = User.find(session[:user_id])
